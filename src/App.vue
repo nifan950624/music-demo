f68dd3bf068bd8637a40fef2b1e7dd8d39ee6176
nifan950624 <nifan1950333426@qq.com>
2020-10-21 09:10:52 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,17 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+   <HelloWorld/>
+    <router-view />
   </div>
 </template>
+<script lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({ components: { HelloWorld } })
+export default class App extends Vue {}
+</script>
 
 <style>
 #app {
---
+++ src/components/topNav/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav">
+    <router-link to="/">首页</router-link>
+    <router-link to="/about">探索</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+export default class topNav extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+}
+</style>
